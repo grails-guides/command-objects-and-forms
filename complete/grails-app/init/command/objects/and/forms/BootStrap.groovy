644e87673681fb3fb02feb06b6d337d467a7fb9a
Matthew D Moss <mattmoss@splatbang.com>
2016-12-27 14:24:50 -0600

    Sample player data.

--- complete/grails-app/init/command/objects/and/forms/BootStrap.groovy
+++ complete/grails-app/init/command/objects/and/forms/BootStrap.groovy
@@ -1,9 +1,17 @@
 package command.objects.and.forms
 
+import demo.Player
+
 class BootStrap {
 
     def init = { servletContext ->
+        [[name: 'Matthew Moss', game: 'Agricola', region: 'East', wins: 12, losses: 4],
+         [name: 'Jeff Brown', game: 'Power Grid', region: 'Central', wins: 9, losses: 2],
+         [name: 'Lindsay Moss', game: 'Shadows Over Camelot', wins: 13, losses: 11]].each {
+            new Player(it).save()
+        }
     }
+
     def destroy = {
     }
 }
