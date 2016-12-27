89b20e9096935d2b4f1b535ac93ea307c50980bf
Matthew D Moss <mattmoss@splatbang.com>
2016-12-27 14:24:39 -0600

    New domain class 'Player' with properties and constraints.

---
+++ complete/grails-app/domain/demo/Player.groovy
@@ -0,0 +1,19 @@
+package demo
+
+class Player {
+
+    String name
+    String game
+    String region
+    int wins = 0
+    int losses = 0
+
+    static constraints = {
+        name blank: false, unqiue: true
+        game blank: false
+        region nullable: true
+        wins min: 0
+        losses min: 0
+    }
+
+}
