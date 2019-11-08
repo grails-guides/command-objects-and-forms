aee91382be5946faa4ac91c79d43645aaa273c78
Sergio del Amo <sergio.delamo@softamo.com>
2019-11-08 18:18:38 +0100

    checkstyle for test

--- complete/src/integration-test/groovy/demo/PlayerControllerFuncSpec.groovy
+++ complete/src/integration-test/groovy/demo/PlayerControllerFuncSpec.groovy
@@ -5,34 +5,39 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
+import spock.lang.AutoCleanup
+import spock.lang.Shared
 import spock.lang.Specification
 import grails.testing.mixin.integration.Integration
 
 @SuppressWarnings(['LineLength', 'MethodName'])
 @Integration
 class PlayerControllerFuncSpec extends Specification {
-
+    @SuppressWarnings(['JUnitPublicProperty']
     @Shared
     @AutoCleanup
     HttpClient client
 
+    @SuppressWarnings(['JUnitPublicNonTestMethod'])
     @OnceBefore
     void init() {
         String baseUrl = "http://localhost:$serverPort"
         this.client  = HttpClient.create(baseUrl.toURL())
     }
 
-    def 'test save validation'() {
+    @SuppressWarnings(['JUnitTestMethodWithoutAssert'])
+    void 'test save validation'() {
         when:
-
         Map<String, Object> json = [name : 'Bob Smith',
                                     wins : 42,
-                                    losses : 'abc']
-        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.POST("/player/save", json), Map)
-               then:
+                                    losses : 'abc',]
+        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.POST('/player/save', json), Map)
+
+        then:
         resp.status == HttpStatus.UNPROCESSABLE_ENTITY // <4>
         resp.body().errors.size() == 2
         resp.body().errors.find { it.field == 'losses' }.message == 'Property losses is type-mismatched'
-        resp.body().errors.find { it.field == 'game' }.message == 'Property [game] of class [class demo.Player] cannot be null'
+        resp.body().errors.find { it.field == 'game' }.message ==
+                'Property [game] of class [class demo.Player] cannot be null'
     }
 }
